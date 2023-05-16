2834ba632e61693fb77c038340a08541178bd049
KRNTVL <kmezaolivos@gmail.com>
2023-05-16 16:58:20 -0400

    Seccion 3: Controlador de Archivos

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,13 +1,22 @@
-const {storageModel} = require("../models");
+const fs = require("fs")
+const { matchedData } = require("express-validator");
+const { storageModel} = require("../models");
+const { handleHttpError } = require("../utils/handleError");
+
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 /**
  * Obtener lista de la base de datos!
  * @param {*} req
  * @param {*} res
  */
 const getItems = async (req, res) => {
-    const data = await storageModel.find({});
-    res.send({data});
+    try{
+        const data = await storageModel.find({});
+        res.send({data});
+    }catch(e){
+        handleHttpError(res, "ERROR_LIST_ITEMS")
+    }  
 };
 /**
  * Obtener un detalle
@@ -15,6 +24,13 @@ const getItems = async (req, res) => {
  * @param {*} res
  */
 const getItem = async (req, res) => {
+    try{
+        const { id } = matchedData(req);
+        const  data = await storageModel.findById(id);
+        res.send({ data });
+    } catch(e){
+        handleHttpError(res, "ERROR_DETAIL_ITEM")
+    }
 };
 /**
  * Insertar un registro
@@ -22,13 +38,17 @@ const getItem = async (req, res) => {
  * @param {*} res
  */
 const createItem = async (req, res) => {
-    const {body, file} = req
-    const fileData = {
-        filename: file.filename,
-        url:`${PUBLIC_URL}/${file.filename}}`
+    try{
+        const {body, file} = req
+        const fileData = {
+            filename: file.filename,
+            url:`${PUBLIC_URL}/${file.filename}}`
+        }
+        const data = await storageModel.create(fileData);
+        res.send({data});
+    } catch(e){
+        handleHttpError(res, "ERROR_CREATE_ITEMS")
     }
-    const data = await storageModel.create(fileData);
-    res.send({data});
 };
 /**
  *  Actualizar un registro
@@ -43,5 +63,23 @@ const updateItem = async (req, res) => {
  * @param {*} res
  */
 const deleteItem = async (req, res) => {
+    try{
+        const { id } = matchedData(req);
+        const dataFile = await storageModel.findById(id);
+        await storageModel.delete({_id:id});
+        const {filename} = dataFile;
+        const filePath = `${MEDIA_PATH}/${filename}`
+        
+        //fs.unlinkSync(filePath);
+        const data = {
+            filePath,
+            deleted: 1
+        }
+        res.send(data);
+    } catch (e) {
+        console.log(e)
+        handleHttpError(res, "ERROR_DELETE_ITEM")
+    }
 };
+
 module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
--- routes/storage.js
+++ routes/storage.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const uploadMiddleware = require("../utils/handleStorage")
-const { createItem } = require("../controllers/storage")
+const { validatorGetItem } = require("../validators/storage")
+const { createItem, getItems, getItem, updateItem, deleteItem } = require("../controllers/storage");
+
 //TODO http://localhost:3000/storage
 
+router.get("/", getItems);
+
+/**Obtener detalle de item*/
+router.get("/:id", validatorGetItem, getItem);
+
+/**Eliminar un registro */
+router.delete("/:id", validatorGetItem, deleteItem);
 
 router.post("/", uploadMiddleware.single("myfile"), createItem)
 
